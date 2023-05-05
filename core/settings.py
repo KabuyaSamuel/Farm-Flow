2c24b8ebb18e135000b4f6f2602df71c83a32528
FeverCode <giddylancs.gl@gmail.com>
2023-05-05 03:34:48 +0300

    Update deployment settings

--- core/settings.py
+++ core/settings.py
@@ -97,7 +97,7 @@ if config('MODE')=="dev":
            'USER': config('DB_USER'),
            'PASSWORD': config('DB_PASSWORD'),
            'HOST': config('DB_HOST'),
-           'PORT': '7449',
+           'PORT': '6146',
        }
        
    }
