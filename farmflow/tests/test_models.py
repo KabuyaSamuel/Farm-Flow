e0c1f0da20b7050ad4c483ea1a80c5befa27a3b1
KabuyaSamuel <samuel.mwangikabuya@kibo.school>
2023-06-11 07:12:01 +0300

    Add Tests - InputUsedModelTest

--- farmflow/tests/test_models.py
+++ farmflow/tests/test_models.py
@@ -1,32 +1,90 @@
+from django.contrib.auth.models import User
+from django.db.utils import IntegrityError
+from django.core.exceptions import ValidationError
+from datetime import datetime
 from django.test import TestCase
-from ..models import Cluster, ProducerGroup, ValueChainChoice
+from ..models import (
+    Cluster, ProducerGroup, ValueChainChoice, SoilTestResult, InputUsed,
+    FarmingType, WaterSource, Farm, Profile, Crop, FarmInputUsed,
+    CropProductionStage, Produce, Tag
+)
 
 
-class ModelsTestCase(TestCase):
+class ClusterModelTest(TestCase):
+    def test_cluster_creation(self):
+        cluster = Cluster.objects.create(name="Test Cluster", location="Test Location")
+        self.assertEqual(cluster.name, "Test Cluster")
+        self.assertEqual(cluster.location, "Test Location")
+        self.assertEqual(str(cluster), "Test Cluster")
+
+class ProducerGroupModelTest(TestCase):
     def setUp(self):
-        # Create a sample Cluster instance
-        self.cluster = Cluster.objects.create(
-            name='Sample Cluster',
-            location='Sample Location'
+        self.cluster = Cluster.objects.create(name="Test Cluster", location="Test Location")
+
+    def test_producer_group_creation(self):
+        producer_group = ProducerGroup.objects.create(name="Test Group", cluster=self.cluster)
+        self.assertEqual(producer_group.name, "Test Group")
+        self.assertEqual(producer_group.cluster, self.cluster)
+        self.assertEqual(str(producer_group), "Test Group (Test Cluster)")
+
+
+class ValueChainChoiceModelTest(TestCase):
+    def test_value_chain_choice_creation(self):
+        value_chain_choice = ValueChainChoice.objects.create(name="Poultry")
+        self.assertEqual(value_chain_choice.name, "Poultry")
+        self.assertEqual(str(value_chain_choice), "Poultry")
+
+class SoilTestResultModelTest(TestCase):
+    def test_soil_test_result_creation(self):
+        soil_test_result = SoilTestResult.objects.create(type="Complete", done="Y")
+        self.assertEqual(soil_test_result.type, "Complete")
+        self.assertEqual(soil_test_result.done, "Y")
+        self.assertEqual(str(soil_test_result), "Complete - Y")
+
+
+    def test_soil_test_result_complete_with_reason(self):
+        soil_test_result = SoilTestResult.objects.create(type="Complete", done="Y", reason="Test Reason")
+        self.assertEqual(soil_test_result.type, "Complete")
+        self.assertEqual(soil_test_result.done, "Y")
+        self.assertEqual(soil_test_result.reason, "Test Reason")
+        self.assertEqual(str(soil_test_result), "Complete - Y")
+
+    def test_soil_test_result_unique_constraint(self):
+        # Create a soil test result with the same type and "done" value
+        SoilTestResult.objects.create(type="Complete", done="Y")
+        
+        # Attempt to create another soil test result with the same values
+        with self.assertRaises(IntegrityError):
+            SoilTestResult.objects.create(type="Complete", done="Y")
+
+class InputUsedModelTest(TestCase):
+    def setUp(self):
+        self.value_chain = ValueChainChoice.objects.create(name="Poultry")
+        
+    def test_input_used_creation(self):
+        input_used = InputUsed.objects.create(
+            type="Fertilizer",
+            value_chain=self.value_chain,
+            name="Nitrogen",
+            amount="kg"
+        )
+        self.assertEqual(input_used.type, "Fertilizer")
+        self.assertEqual(input_used.value_chain, self.value_chain)
+        self.assertEqual(input_used.name, "Nitrogen")
+        self.assertEqual(input_used.amount, "kg")
+        self.assertEqual(
+            str(input_used),
+            "Nitrogen (kg of Fertilizer)"
         )
 
-        # Create a sample ProducerGroup instance
-        self.producer_group = ProducerGroup.objects.create(
-            name='Sample Producer Group',
-            cluster=self.cluster
+    def test_input_used_str_representation(self):
+        input_used = InputUsed(
+            type="Pesticides",
+            value_chain=self.value_chain,
+            name="Insecticide",
+            amount="litres"
         )
-
-        # Create a sample ValueChainChoice instance
-        self.value_chain_choice = ValueChainChoice.objects.create(
-            name='Sample Value Chain Choice'
+        self.assertEqual(
+            str(input_used),
+            "Insecticide (litres of Pesticides)"
         )
-
-    def test_cluster_str(self):
-        self.assertEqual(str(self.cluster), 'Sample Cluster')
-
-    def test_producer_group_str(self):
-        expected_str = 'Sample Producer Group (Sample Cluster)'
-        self.assertEqual(str(self.producer_group), expected_str)
-
-    def test_value_chain_choice_str(self):
-        self.assertEqual(str(self.value_chain_choice), 'Sample Value Chain Choice')
