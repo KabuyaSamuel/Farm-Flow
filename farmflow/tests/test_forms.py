c7d99d45ab27ae17c75dbf81f56487a5d6da6cc1
KabuyaSamuel <samuel.mwangikabuya@kibo.school>
2023-06-11 23:38:09 +0300

    Forms Tests - valid & invalid form test

---
+++ farmflow/tests/test_forms.py
@@ -0,0 +1,31 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from ..forms import RegisterForm, LoginForm, UpdateUserForm, UpdateProfileForm, PlantImageForm
+from ..models import Profile
+
+
+class RegisterFormTest(TestCase):
+    def test_form_valid(self):
+        form_data = {
+            'first_name': 'John',
+            'last_name': 'Doe',
+            'username': 'johndoe',
+            'email': 'johndoe@example.com',
+            'password1': 'mypassword123',
+            'password2': 'mypassword123'
+        }
+        form = RegisterForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+    def test_form_invalid(self):
+        form_data = {
+            'first_name': 'John',
+            'last_name': 'Doe',
+            'username': 'johndoe',
+            'email': 'invalidemail',  # Invalid email format
+            'password1': 'mypassword123',
+            'password2': 'mypassword123'
+        }
+        form = RegisterForm(data=form_data)
+        self.assertFalse(form.is_valid())
+        self.assertIn('email', form.errors)
